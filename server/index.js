4e231e0abaa3762d6adeca8c3cd8a3a867682cab
Matúš Holotňák <matus.holotnak@gmail.com>
2022-07-21 01:08:06 +0200

    create routes

---
+++ server/index.js
@@ -0,0 +1,80 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+require("dotenv").config();
+const pool = require("./db/db");
+const {
+  server: { PORT },
+} = require("./config");
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+// ROUTES
+
+//create a todo
+app.post("/todos", async (req, res) => {
+  try {
+    const { description } = req.body;
+    const newTodo = await pool.query(
+      "INSERT INTO todo (description) VALUES ($1) RETURNING *",
+      [description]
+    );
+    res.json(newTodo.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+//get all todos
+app.get("/todos", async (req, res) => {
+  try {
+    const todos = await pool.query("SELECT * FROM todo");
+    res.json(todos.rows);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+//get a todo
+app.get("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const todo = await pool.query("SELECT * FROM todo WHERE tid = $1", [id]);
+    res.json(todo.rows[0]);
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+//update a todo
+app.put("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { description } = req.body;
+    const updateTodo = await pool.query(
+      "UPDATE todo SET description = $1 WHERE tid = $2",
+      [description, id]
+    );
+    res.json("Todo was updated.");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+//delete a todo
+app.delete("/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deleteTodo = await pool.query("DELETE FROM todo WHERE tid = $1", [
+      id,
+    ]);
+    res.json("Todo was deleted.");
+  } catch (error) {
+    console.error(error.message);
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`server has started on port ${PORT}`);
+});
